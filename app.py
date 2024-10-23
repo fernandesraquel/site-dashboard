8b8ca617e2abad0d8506a3e03dbd675283b47737
Raquel Fernandes <94188045+fernandesraquel@users.noreply.github.com>
2024-10-22 21:48:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,8 @@ app.layout = html.Div([
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run_server(host='0.0.0.0', port=port)
 
 
 
